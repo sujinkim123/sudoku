85834b1848e756228abda27dee314b56928a9084
sujinkim123 <alicekim0429@naver.com>
2021-09-01 16:25:56 +0900

    혼자하는프로젝트

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module sample.sudoku {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens sudoku to javafx.fxml;
+    exports sudoku;
+}
---
+++ src/main/java/sudoku/SudokuApplication.java
@@ -0,0 +1,26 @@
+package sudoku;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import sudoku.buildlogic.SudokuBuildLogic;
+import sudoku.userinterface.IUserInterfaceContract;
+import sudoku.userinterface.UserInterfaceImpl;
+
+public class SudokuApplication extends Application {
+    private IUserInterfaceContract.View uiImpl;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        uiImpl = new UserInterfaceImpl(primaryStage);
+        try {
+            SudokuBuildLogic.build(uiImpl);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
